1d1e74b17f2d4400c9f617603d3b5d7e725e4d37
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-21 01:05:44 +0400

    Added divide negative

--- tests/test_divide_negative.py
+++ tests/test_divide_negative.py
@@ -3,5 +3,5 @@ from qa_automation_cource.basic_calc import BasicCalc
 
 class TestMultiply:
     def test_divide(self):
-        result = BasicCalc.calc_divide(0, 1)
+        result = BasicCalc.calc_divide(10, 0)
         assert result == 0
