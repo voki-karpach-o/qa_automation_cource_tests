1277c283745d3c36fa709db1f392a110fe2310f4
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-21 19:34:14 +0400

    Added the test-cases about memo_plus

---
+++ tests/test_new_calc/test_memo_plus_negative.py
@@ -0,0 +1,11 @@
+from qa_automation_cource.new_calc import NewCalc
+
+
+class TestRegular:
+    def test_memo_plus_negative(self):
+        calc = NewCalc()
+        calc.memo_plus(5)
+        calc.memo_plus(10)
+        calc.memo_plus(15)
+        calc.memo_plus(20)
+        assert len(calc.memory) == 3, f'Ожидал, что добавится не больше 3 значений и результат {len(calc.memory)}'
