e27a0964c19f35859d430c213875356cc2c6afde
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-21 14:20:48 +0400

    Added subtrack positive

--- tests/test_subtract_positive.py
+++ tests/test_subtract_positive.py
@@ -1,9 +1,7 @@
-import pytest
-
 from qa_automation_cource.basic_calc import BasicCalc
 
 
 class TestMultiply:
     def test_subtract(self):
-        with pytest.raises(ValueError):
-            BasicCalc.calc_subtract(0, 0)
+        result = BasicCalc.calc_subtract(0, 0)
+        assert result == 0, 'Введите другие значения'
