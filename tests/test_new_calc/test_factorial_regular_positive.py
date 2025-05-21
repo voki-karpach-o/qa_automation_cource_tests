312cd2eac4a0dc1bc4bd9722ab2776388271699f
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-21 19:17:53 +0400

    Fixed the code

--- tests/test_new_calc/test_factorial_regular_positive.py
+++ tests/test_new_calc/test_factorial_regular_positive.py
@@ -1,5 +1,3 @@
-import pytest
-
 from qa_automation_cource.new_calc import factorial_regular
 
 
@@ -7,4 +5,3 @@ class TestRegular:
     def test_factorial_regular_positive(self):
         result = factorial_regular(4)
         assert result == 24, f'Ожидал 24, но результат {result}'
-
