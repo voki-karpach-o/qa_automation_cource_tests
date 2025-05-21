f41eb246e3ab0acce0f5bcceebb2b8403a649069
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-21 22:03:29 +0400

    Added the parametrize in test_divide_positive

--- tests/tests_basic_calc/test_divide_positive.py
+++ tests/tests_basic_calc/test_divide_positive.py
@@ -2,9 +2,23 @@ import pytest
 
 from qa_automation_cource.basic_calc import BasicCalc
 
+'''Почему выбрал функцию деления:
+1.  Большое разнообразие на первый взгляд странных входных данных
+2.  Одна из важных базовых функций которая должна работать правильно
+3.  Важный момент, это проверка специфического кейса при делении на 0   
+'''
+
 
 @pytest.mark.critical
+@pytest.mark.parametrize(
+    "a, b, result",
+    [
+        (1, 2, 0.5),
+        (0, 0, 0),
+        (-1, -1, 1),
+        (6, 2, 3)
+    ]
+)
 class TestDivide:
-    def test_divide(self):
-        result = BasicCalc.calc_divide(9, 3)
-        assert result == 3
+    def test_divide(self, a, b, result):
+        assert BasicCalc.calc_divide(a, b) == result
