1c47f1eb9cbc372c1a845bb1ce4c45e3443b46cc
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-22 19:05:13 +0400

    1) Added logs in conftest.py 2) Fixed the code in new_calc.py

--- conftest.py
+++ conftest.py
@@ -18,3 +18,12 @@ def calculator_new_with_memory():
     calc = NewCalc()
     calc.memo_plus(5)
     return calc
+
+
+@pytest.fixture
+def temp_log_file(tmpdir):
+    log_file = tmpdir.join("calculator_log.txt")
+    yield log_file
+    if os.path.exists(log_file):
+        os.remove(log_file)
+        
