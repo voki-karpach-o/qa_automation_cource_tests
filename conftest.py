1e6f4417683a54a1d927cd4058e63081c136e881
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-22 16:11:02 +0400

    Added the fixture in conftest.py

--- conftest.py
+++ conftest.py
@@ -1,4 +1,12 @@
 import sys
 import os
+import pytest
+from qa_automation_cource.basic_calc import BasicCalc
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+
+@pytest.fixture
+def calculator_basic():
+    calc = BasicCalc()
+    return calc
