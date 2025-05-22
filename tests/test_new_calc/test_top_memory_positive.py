bffb2b8dffab1bf541fcced7fe6dff9192ad5f0b
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-22 18:15:34 +0400

    Fixed the test_top_memory_positive.py

--- tests/test_new_calc/test_top_memory_positive.py
+++ tests/test_new_calc/test_top_memory_positive.py
@@ -1,9 +1,4 @@
-from qa_automation_cource.new_calc import NewCalc
-
-
 class TestTopMemory:
-    def test_top_memory_positive(self):
-        calc = NewCalc()
-        calc.memo_plus(5)
-        result = calc.top_memory
+    def test_top_memory_positive(self, calculator_new_with_memory):
+        result = calculator_new_with_memory.top_memory
         assert result == 5, f"Ожидаю, что будет возвращено 5 и результат {result}"
