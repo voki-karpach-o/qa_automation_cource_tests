119ad8de16ff0153df8a2cd1f13b08ba16558877
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-28 22:18:40 +0400

    1) Combined top memory negative and top memory positive into one file

--- tests/test_new_calc/test_top_memory.py
+++ tests/test_new_calc/test_top_memory.py
@@ -7,3 +7,8 @@ class TestTopMemory:
         calc.memo_plus(5)
         result = calc.top_memory
         assert result == 5, f"Ожидаю, что будет возвращено 5 и результат {result}"
+
+    def test_top_memory_negative(self):
+        calc = NewCalc()
+        result = calc.top_memory
+        assert result == 'Список пуст!', f"Ожидаю, что будет 'Список пуст!' и результат {result}"
