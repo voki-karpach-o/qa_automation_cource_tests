c53682bcd929c68843ae330707976e5d2ac9570c
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-29 19:59:57 +0400

    1) Combined cases about test_add 2) Changed the name of the test with the bug

--- tests/tests_basic_calc/test_add_positive.py
+++ tests/tests_basic_calc/test_add_positive.py
@@ -5,6 +5,11 @@ from qa_automation_cource.basic_calc import BasicCalc
 
 @pytest.mark.critical
 class TestAdd:
-    def test_add(self):
-        result = BasicCalc.calc_add(5, 3)
-        assert result == 8
+    def test_add_positive(self):
+        current_result = BasicCalc.calc_add(5, 3)
+        expected_result = 8
+        assert current_result == expected_result, f'Неправильное значение для сложения {current_result}, ожидал {expected_result}'
+
+    def test_add_negative(self):
+        with pytest.raises(TypeError, match="Одним из значений является строка"):
+            BasicCalc.calc_add('A', 3)
