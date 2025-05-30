9ce29c58f7b386ff0be072ab10d2c875acf4d542
Карпач Олег <oleg.karpach@vokigames.com>
2025-05-30 20:37:02 +0400

    1) Combined cases about test_multiply 2) Combined cases about test_subtract

--- tests/tests_basic_calc/test_multiply_negative.py
+++
@@ -1,7 +0,0 @@
-from qa_automation_cource.basic_calc import BasicCalc
-
-
-class TestMultiply:
-    def test_multiply(self):
-        result = BasicCalc.calc_multiply('', 3)
-        assert result == '', "Умножение пустой строки на число вернёт пустую строку"
--- tests/tests_basic_calc/test_subtract_positive.py
+++ tests/tests_basic_calc/test_subtract_positive.py
@@ -5,6 +5,11 @@ from qa_automation_cource.basic_calc import BasicCalc
 
 @pytest.mark.critical
 class TestSubtract:
-    def test_subtract(self):
-        result = BasicCalc.calc_subtract(5, 3)
-        assert result == 2
+    def test_subtract_positive(self):
+        current_result = BasicCalc.calc_subtract(5, 3)
+        expected_result = 2
+        assert current_result == expected_result, f'Неправильное значение для вычитания {current_result}, ожидал {expected_result}'
+
+    def test_subtract_negative(self):
+        with pytest.raises(TypeError, match="Одним из значений является строка"):
+            BasicCalc.calc_add('A', 3)
