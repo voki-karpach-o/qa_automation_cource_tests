d8b8839da5f71bb0fdae065826db9e034ca6de81
Карпач Олег <oleg.karpach@vokigames.com>
2025-06-22 12:37:39 +0400

    1) Renamed tests 2) Added a logging fixture 3) Added a fixture about memory 4) Added a tests about memory 5) Added a tests about logging

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 import sys
 import os
+from datetime import datetime
+
 import pytest
 from qa_automation_cource.basic_calc import BasicCalc
 from qa_automation_cource.new_calc import NewCalc
@@ -22,15 +24,36 @@ def calculator_new():
 
 @pytest.fixture
 def calculator_with_memory(calculator_new):
-    return calculator_new.memo_plus(5)
+    calculator_new.memo_plus(5)
+    return calculator_new
 
 
 @pytest.fixture
-def temp_log_file(tmpdir):
-    log_file = tmpdir.join("calculator_log.txt")
-    yield log_file
-    if os.path.exists(log_file):
-        os.remove(log_file)
+def temp_log_file(tmp_path):
+    log_file_path = tmp_path / "calculator_log.txt"
+    return log_file_path
+
+
+@pytest.fixture
+def calculator_with_temp_log(monkeypatch, temp_log_file):
+    def fake_log_operation(self, operation_type, arguments, result_val):
+        date_logging = datetime.now().date().strftime("%Y.%m.%d")
+        time_logging = datetime.now().time().strftime("%H:%M:%S")
+        log_entry = {
+            "Операция": operation_type,
+            "Аргументы": arguments,
+            "Результат": result_val,
+            "Дата": date_logging,
+            "Время": time_logging
+        }
+        with open(temp_log_file, "a", encoding="utf-8") as log_file_op:
+            log_file_op.write(str(log_entry) + "\n")
+
+    monkeypatch.setattr(NewCalc, 'log_operation', fake_log_operation)
+
+    calc = NewCalc()
+    calc.test_log_file = temp_log_file
+    return calc
 
 
 @pytest.fixture
--- tests/test_new_calc/test_top_memory_new.py
+++ tests/test_new_calc/test_top_memory_new.py
@@ -1,5 +1,12 @@
 class TestTopMemory:
     def test_top_memory_positive(self, calculator_with_memory):
-        current_result = calculator_with_memory
+        current_result = calculator_with_memory.top_memory
         expected_result = 5
         assert current_result == expected_result, f"Неправильное значение для верхнего значения в памяти, ожидал {expected_result} а результат {current_result}"
+
+    def test_top_memory_and_logging(self, calculator_with_temp_log):
+        calc = calculator_with_temp_log
+        calc.num_1 = "12+8"
+        calc.calculate_result()
+        log_content = calc.test_log_file.read_text(encoding="utf-8")
+        print(f"Содержимое лога: {log_content}")
