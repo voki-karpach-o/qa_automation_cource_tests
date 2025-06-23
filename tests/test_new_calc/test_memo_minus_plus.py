18dc54754befc962bbec218715c06657cf7c2570
Карпач Олег <oleg.karpach@vokigames.com>
2025-06-23 23:46:25 +0400

    1) Fixed the tests from test_memo_minus_plus.py

--- tests/test_new_calc/test_memo_minus_plus.py
+++ tests/test_new_calc/test_memo_minus_plus.py
@@ -6,14 +6,15 @@ from qa_automation_cource.new_calc import NewCalc
 class TestMemoMinus:
     def test_memo_minus_positive(self):
         calc = NewCalc()
-        calc.memo_plus(5)
-        calc.memo_plus(10)
+        current_result = calc.memory = [5, 10, 15]
         calc.memo_minus()
-        current_result = calc.memory
-        expected_result = [5]
-        assert current_result == expected_result, f"Неправильное значение для верхнего значения в памяти, ожидал {expected_result} а результат {current_result}"
+        assert current_result == [5, 10], "Позитивный кейс: удаление значения из памяти"
 
     def test_memo_minus_negative(self):
+        """
+            Проверяет, что memo_minus выбрасывает ValueError при попытке
+            извлечь значение из пустой памяти.
+        """
         calc = NewCalc()
         with pytest.raises(ValueError):
             calc.memo_minus()
@@ -25,14 +26,12 @@ class TestMemoPlus:
         calc.memo_plus(5)
         current_result = calc.memory
         expected_result = [5]
-        assert current_result == expected_result, f"Неправильное значение для верхнего значения в памяти, ожидал {expected_result} а результат {current_result}"
+        assert current_result == expected_result, f"Позитивный кейс: добавление значение в память {calc.memory}"
 
     def test_memo_plus_negative(self):
         calc = NewCalc()
-        calc.memo_plus(5)
-        calc.memo_plus(10)
-        calc.memo_plus(15)
-        calc.memo_plus(20)
+        calc.memory = [5, 10, 15, 20]
         current_result = len(calc.memory)
         expected_result = 3
-        assert current_result == expected_result, f'Ожидал, что добавится не больше 3 значений а добавилось {current_result}'
+        assert current_result == expected_result, (f'Негативный кейс: превышено количество значений в памяти '
+                                                   f' {calc.memory}')
