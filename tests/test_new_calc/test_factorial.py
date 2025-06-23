84cb9854a26943a948c7fc50f1daa500d8314306
Карпач Олег <oleg.karpach@vokigames.com>
2025-06-23 23:12:16 +0400

    1) Fixed the tests from test_factorial.py

--- tests/test_new_calc/test_factorial.py
+++ tests/test_new_calc/test_factorial.py
@@ -7,10 +7,14 @@ class TestRecursive:
     def test_factorial_recursive_positive(self):
         current_result = factorial_recursive(5)
         expected_result = 120
-        assert current_result == expected_result, f'Неправильное значение для факториала {factorial_recursive}, ожидал {expected_result}'
+        assert current_result == expected_result, (f'Позитивный кейс: вычисление рекурсивного факториала для числа '
+                                                   f' {current_result}')
 
-    # Проверяем, что при слишком большом числе (2500) произойдёт ошибка переполнения стека рекурсии
     def test_factorial_recursive_negative(self):
+        """
+            Проверяем, что рекурсивный факториал падает с RecursionError
+            при попытке вычислить значение для слишком большого числа.
+        """
         with pytest.raises(RecursionError):
             factorial_recursive(2500)
 
@@ -19,8 +23,13 @@ class TestRegular:
     def test_factorial_regular_positive(self):
         current_result = factorial_regular(4)
         expected_result = 24
-        assert current_result == expected_result, f'Неправильное значение для факториала {factorial_recursive}, ожидал {expected_result}'
+        assert current_result == expected_result, (f'Негативный кейс: вычисление рекурсивного факториала для числа'
+                                                   f'  {current_result}')
 
     def test_factorial_regular_negative(self):
-        with pytest.raises(RecursionError):
+        """
+            Проверяем, что рекурсивный факториал падает с TypeError
+            при попытке вычислить значение для буквы.
+        """
+        with pytest.raises(TypeError):
             factorial_regular('a')
